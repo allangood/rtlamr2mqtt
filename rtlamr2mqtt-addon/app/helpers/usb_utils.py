3ee627b78d3fd628d5ef3b4d5902f8517ce3f796
Allan Gomes GooD <allan.cassaro@gmail.com>
2025-05-30 09:10:00 -0500

    issue-226 Convert usbdev and usbnum to int (#267)

--- rtlamr2mqtt-addon/app/helpers/usb_utils.py
+++ rtlamr2mqtt-addon/app/helpers/usb_utils.py
@@ -41,7 +41,7 @@ def reset_usb_device(usbdev):
     Reset USB port
     """
     if usbdev is not None and ':' in usbdev:
-        busnum, devnum = usbdev.split(':')
+        busnum, devnum = [int(x) for x in usbdev.split(':')]
         filename = f"/dev/bus/usb/{busnum:03d}/{devnum:03d}"
         if os.path.exists(filename) and S_ISCHR(os.stat(filename).st_mode):
             #define USBDEVFS_RESET_IO('U', 20)
