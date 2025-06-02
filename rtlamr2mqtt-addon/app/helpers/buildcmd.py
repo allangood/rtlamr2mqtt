cdc8352d60b14185f2172a4aeb0a98892c0ca360
Allan GooD <allan.cassaro@gmail.com>
2025-06-02 22:17:50 +0000

    274 Fix hang rtl_tcp

--- rtlamr2mqtt-addon/app/helpers/buildcmd.py
+++ rtlamr2mqtt-addon/app/helpers/buildcmd.py
@@ -11,7 +11,6 @@ def get_comma_separated_str(key, list_of_dict):
     c = []
     for d in list_of_dict:
         if key in list_of_dict[d]:
-            print(list_of_dict[d][key])
             c.append(str(list_of_dict[d][key]))
     return ','.join(c)
 
@@ -44,14 +43,14 @@ def build_rtlamr_args(config):
     rtltcp_host = [ f'-server={config["general"]["rtltcp_host"]}' ]
     if 'rtlamr' in config['custom_parameters']:
         custom_parameters = [ config['custom_parameters']['rtlamr'] ]
+        custom_parameters = partial_match_remove('server', custom_parameters)
     else:
         custom_parameters = [ '-unique=true' ]
-    default_args = partial_match_remove('server', default_args)
 
     # Build a comma-separated string of meter IDs
     ids = ','.join(list(meters.keys()))
     filterid_arg = [ f'-filterid={ids}' ]
-    
+
     # Build a comma-separated string of message types
     msgtypes = get_comma_separated_str('protocol', meters)
     msgtype_arg = [ f'-msgtype={msgtypes}' ]
@@ -68,6 +67,8 @@ def build_rtltcp_args(config):
     """
     # Build the command line arguments for the rtlamr command
     # based on the configuration file
+    if config["general"]["rtltcp_host"].split(':')[0] not in [ '127.0.0.1', 'localhost' ]:
+        return None
     custom_parameters = ''
     if 'rtltcp' in config['custom_parameters']:
         custom_parameters = config['custom_parameters']['rtltcp']
